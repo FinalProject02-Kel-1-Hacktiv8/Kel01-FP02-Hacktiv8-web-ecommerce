3e2c25d79ba9a556f790506b17007f409e8dd13c
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-17 23:27:07 +0700

    Modified: Add FontAwesome for icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://kit.fontawesome.com/0ae515efac.js"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
