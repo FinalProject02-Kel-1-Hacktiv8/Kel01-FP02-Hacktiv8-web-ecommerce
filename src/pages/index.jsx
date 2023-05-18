836b8930f1d1e66190194a6c977b9fefaf576922
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-18 23:06:29 +0700

    Modified: Update home for using the Card Details

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,5 @@
 import { Card } from "@/components/Card";
-import Detail from "@/components/CardDetails/Detail";
-import Navbar from "@/components/Navbar";
+import CardDetail from "@/components/CardDetails";
 import { fetchDataProduct } from "@/redux/slice/slice-products";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,45 +8,44 @@ export default function Home() {
   const dispatch = useDispatch();
   const homeProducts = useSelector((state) => state.products);
 
-  // let title = homeProducts.productDetails;
-
   // let arrayHomeProducts = Object.keys(homeProducts);
 
   useEffect(() => {
     dispatch(fetchDataProduct("/products"));
   }, [dispatch]);
 
-  console.log("data", homeProducts);
+  // console.log("data", carts);
   return (
-    <div className="container mx-auto max-w-[1000px] mt-5">
-      <Navbar />
-
+    <div className="grid grid-cols-4 gap-5 mt-32">
       {/* CARDS */}
-      <div className="grid grid-cols-4 gap-5 mt-32">
-        {homeProducts.data.map((product) => {
-          return (
-            <>
-              <Card
-                key={product.id}
-                id={product.title}
-                title={product.title}
-                category={product.category}
-                src={product.image}
-                price={product.price}
-                rate={product.rating.rate}
-                count={product.rating.count}
-              />
-              {/* THIS IS THE MODAL CARDS PLACE FOR DETAILS */}
-              <Detail
-                title={product.title}
-                src={product.image}
-                description={product.description}
-                category={product.category}
-              />
-            </>
-          );
-        })}
-      </div>
+      {homeProducts.data.map((product, id) => {
+        return (
+          <>
+            <Card
+              key={product.id}
+              id={product.title}
+              title={product.title}
+              category={product.category}
+              src={product.image}
+              price={product.price}
+              rate={product.rating.rate}
+              count={product.rating.count}
+            />
+            {/* THIS IS THE MODAL CARDS PLACE FOR DETAILS */}
+            <CardDetail
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              src={product.image}
+              description={product.description}
+              category={product.category}
+              price={product.price}
+              rate={product.rating.rate}
+              count={product.rating.count}
+            />
+          </>
+        );
+      })}
     </div>
   );
 }
