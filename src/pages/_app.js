c2905de2051339e03e3608418317d877fceb0b4b
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-18 23:00:29 +0700

    Modified: Update app.jsx for using the Layout.jsx

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import { wrapper } from "@/redux/store";
 import { Provider } from "react-redux";
+import { Layout } from "@/components/Layout";
 
 export default function App({ Component, ...rest }) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
-      <Component {...props} />
+      <Layout>
+        <Component {...props} />
+      </Layout>
     </Provider>
   );
 }
