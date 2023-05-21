d5454b221f10e30c1a094fff5b13dafbfe535c83
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-21 23:55:36 +0700

    UI: Update Navbar logo and layout

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Navlink from "./Navlink";
+import NavEnd from "./NavEnd";
+import { useSelector } from "react-redux";
+
+export default function NavbarComponent() {
+  const { token } = useSelector((state) => state.users);
+
+  return (
+    <nav className="navbar bg-base-100 p-0 flex items-center justify-center sticky top-0 z-30 md:h-[60px] h-[110px] w-full">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <Navlink item="Home" href="/" isMobile />
+          <Navlink item="Shop" href="shop" isMobile />
+          <Navlink item="Calaboration" href="/calaboration" isMobile />
+          <Navlink item="Checkout" href="/checkout" isMobile />
+        </div>
+        <a className="btn btn-ghost normal-case text-xl text-purple-300">
+          <i class="fa-solid fa-bag-shopping pr-2 text-purple-600"></i> JAJAN.id
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <Navlink item="Home" href="/" />
+        <Navlink item="Shop" href="/shop" />
+        <Navlink item="Calaboration" href="/calaboration" />
+        <Navlink item="Checkout" href="/checkout" />
+      </div>
+      <div className="navbar-end">
+        {!token ? <NavEnd /> : <NavEnd isToken />}
+      </div>
+    </nav>
+  );
+}
