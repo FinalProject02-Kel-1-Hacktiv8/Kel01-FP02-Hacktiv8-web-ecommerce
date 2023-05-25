bd0dc7a3b9a6e9589bf128082fcf49bc4ff2acda
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-25 15:59:01 +0700

    Update Cart-summary.jsx

--- src/components/Payment/Summary/cart-summary.jsx
+++ src/components/Payment/Summary/cart-summary.jsx
@@ -22,7 +22,7 @@ export const CartSummary = ({ item }) => {
     "https://wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found.jpg"
   }`;
   return (
-    <div className="flex justify-between text-base-100 title-font text-md font-semibold py-3 border-b-[1px] border-slate-300">
+    <div className="flex justify-between dark:text-slate-300 text-slate-800 title-font text-md font-semibold py-3 border-b-[1px] border-slate-300">
       <div className="flex">
         <Image
           className="object-contain object-center relative"
@@ -33,16 +33,17 @@ export const CartSummary = ({ item }) => {
           priority={true}
           alt={`Product Image ${item?.title ?? "Product image is not found"}`}
         />
-        <div className="px-5">
+        <div className="px-5 text-base-100">
           <p>{item?.title}</p>
           <p>$ {item?.price}</p>
+          <p>Stock {item?.stock}</p>
         </div>
       </div>
 
       <div className="flex flex-col justify-between items-end">
         <div className="btn-group">
           <button
-            className="btn btn-sm text-lg bg-base-100"
+            className="btn btn-sm text-lg bg-slate-800"
             onClick={(e) => {
               e.preventDefault();
               handleSubtractCart(item.quantity);
@@ -50,9 +51,9 @@ export const CartSummary = ({ item }) => {
           >
             -
           </button>
-          <p className="btn btn-sm bg-base-300 text-white">{item?.quantity}</p>
+          <p className="btn btn-sm bg-slate-800 text-white">{item?.quantity}</p>
           <button
-            className="btn btn-sm text-lg bg-base-100"
+            className="btn btn-sm text-lg bg-slate-800"
             onClick={handleAddCart}
           >
             +
@@ -62,7 +63,7 @@ export const CartSummary = ({ item }) => {
           className="text-lg text-center w-5"
           onClick={() => dispatch(removeCart(item?.id))}
         >
-          <i class="fa-regular fa-trash-can text-slate-500 hover:text-red-500"></i>
+          <i className="fa-regular fa-trash-can text-slate-500 hover:text-red-500"></i>
         </button>
       </div>
     </div>
