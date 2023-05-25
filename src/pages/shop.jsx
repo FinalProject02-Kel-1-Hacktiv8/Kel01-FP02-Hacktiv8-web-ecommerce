fe03df8020ed4ebf7c4af15f53e28b74c87534b5
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-25 15:57:16 +0700

    Update shop.jsx

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,12 +1,29 @@
-import React from "react";
 import Card from "@/components/Card";
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import loadingImage from "/public/isLoading.gif";
 
 export default function Shop() {
+  const { loading } = useSelector((state) => state.products);
+
   return (
-    <div className="container mx-auto max-w-[1000px] mt-5">
+    <section className="container mx-auto max-w-[1000px] mt-5">
       <Navbar />
-      <Card type="shop" />
-    </div>
+      {loading == true ? (
+        <div className="flex flex-col justify-center items-center mt-40">
+          <Image
+            src={loadingImage}
+            width={300}
+            height={300}
+            alt="loading"
+            priority={true}
+          />
+          <h3 className="text-3xl font-bold">Loading!</h3>
+        </div>
+      ) : (
+        <Card type="shop" />
+      )}
+    </section>
   );
 }
