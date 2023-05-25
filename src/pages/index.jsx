ae51c3665c2df242a11041d7dc1ed976dde6d9e6
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-25 15:58:11 +0700

    Update index.jsx for using isLoading

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,20 +4,41 @@ import Navbar from "@/components/Navbar";
 import { fetchDataProduct } from "@/redux/slice/slice-products";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Image from "next/image";
+import loadingImage from "/public/isLoading.gif";
+import { DashboardInfo } from "@/components/Dashboard";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const products = useSelector((state) => state.products);
+  const { loading } = useSelector((state) => state.products);
+  const { token, role } = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(fetchDataProduct("/products"));
   }, [dispatch]);
 
   return (
-    <div className="container mx-auto max-w-[1000px] mt-5">
+    <section className="container mx-auto max-w-[1000px] mt-5">
       <Navbar />
-      <Carousel />
-      <Card />
-    </div>
+      {loading == true ? (
+        <div className="flex flex-col justify-center items-center mt-40">
+          <Image
+            src={loadingImage}
+            width={300}
+            height={300}
+            alt="loading"
+            priority={true}
+          />
+          <h3 className="text-3xl font-bold text-white">Loading!</h3>
+        </div>
+      ) : token && role == "admin" ? (
+        <DashboardInfo />
+      ) : (
+        <>
+          <Carousel />
+          <Card />
+        </>
+      )}
+    </section>
   );
 }
