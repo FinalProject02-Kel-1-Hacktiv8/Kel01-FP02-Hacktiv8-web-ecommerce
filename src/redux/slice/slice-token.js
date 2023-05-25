07e45c79f5e37d8a6ca1ca70d4d3dfc4ad2a4bb1
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-25 15:43:27 +0700

    Update Slice-token and store

--- src/redux/slice/slice-token.js
+++ src/redux/slice/slice-token.js
@@ -1,16 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const adminAccount = {
+  username: "admin",
+  password: "admin123",
+  token: "abcdAdmin123$#",
+  role: "admin",
+};
+
 const sliceUser = createSlice({
   name: "user",
   initialState: {
     token: null,
+    role: null,
   },
   reducers: {
     addToken: (state, action) => {
-      state.token = action.payload;
+      state.token = action.payload.token;
+      state.role = action.payload.role;
     },
     deleteToken: (state) => {
       state.token = null;
+      state.role = null;
     },
   },
 });
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,6 +5,7 @@ import { createWrapper } from "next-redux-wrapper";
 import productsReducers from "../slice/slice-products";
 import userReducers from "../slice/slice-token";
 import cartReducers from "../slice/slice-cart";
+import updateProductReducers from "../slice/slice-update";
 
 const persistConfig = {
   key: "root",
@@ -16,6 +17,7 @@ const rootReducers = combineReducers({
   products: productsReducers,
   users: userReducers,
   cart: cartReducers,
+  updateProduct: updateProductReducers,
 });
 
 const persist = persistReducer(persistConfig, rootReducers);
