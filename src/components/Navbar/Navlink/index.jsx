5832f22ed44f23be4f983d77946541ae3b429e73
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-17 23:59:03 +0700

    feat(NavLink): Update Navlink for mobile responsive

--- src/components/Navbar/Navlink/index.jsx
+++ src/components/Navbar/Navlink/index.jsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import React from "react";
 
-export default function Navlink({ item, href, isMobile }) {
+export default function Navlink({ link, isMobile }) {
   if (isMobile) {
     return (
       <ul
         tabIndex={0}
         className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
       >
-        <li>
-          <Link href={href}>{item}</Link>
-        </li>
+        {link.map((value, index) => (
+          <li>
+            <Link href={value.href} key={index}>
+              {value.item}
+            </Link>
+          </li>
+        ))}
       </ul>
     );
   }
